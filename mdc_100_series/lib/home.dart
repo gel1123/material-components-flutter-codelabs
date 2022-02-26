319a7fd6964a90137d29299ca3ae76729fea8ee8
gel1123 <atno.14@gmail.com>
2022-02-26 09:30:23 +0900

    DONE: Set resizeToAvoidBottomInset (101)

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -29,7 +29,8 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text('You did it!'),
       ),
-      // TODO: Set resizeToAvoidBottomInset (101)
+      // DONE: Set resizeToAvoidBottomInset (101)
+      resizeToAvoidBottomInset: false, // キーボードの外観がホームページまたはそのウィジェットのサイズを変更しないようにする
     );
   }
 }
