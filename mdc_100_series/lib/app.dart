f0e0c2a7f2b66ea338fa5fc6343fcd9e7803d011
gel1123 <atno.14@gmail.com>
2022-02-26 11:35:27 +0900

    DONE: Build a Shrine Text Theme (103)

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -59,8 +59,34 @@ ThemeData _buildShrineTheme() {
       primary: kShrinePink100,
       onPrimary: kShrineBrown900,
       secondary: kShrineBrown900,
-      error: kShrineErrorRed
+      error: kShrineErrorRed,
+    ),
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePink100,
     ),
   );
 }
-// TODO: Build a Shrine Text Theme (103)
+// DONE: Build a Shrine Text Theme (103)
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline5: base.headline5!.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    headline6: base.headline6!.copyWith(
+      fontSize: 18.0,
+    ),
+    caption: base.caption!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    bodyText1: base.bodyText1!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: "Rubik",
+    displayColor: kShrineBrown900,
+    bodyColor: kShrineBrown900,
+  );
+}
